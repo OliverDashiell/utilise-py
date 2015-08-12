8b88a58e6bdda110c89b48dbfc257ecd3c7fd58b
jamesstidard <jamesstidard@gmail.com>
2015-08-12 18:28:31 +0100

    setup amendments

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ version = '0.1'
 
 setup(name='utilise-py',
       version=version,
-      packages=find_packages(exclude=['src.tests*']),
+      packages=find_packages(exclude=['tests*']),
       include_package_data=True,
+      install_requires=['setuptools'])
+
+setup(name='utilise-py',
+      version=version,
+      packages=['utilise'],
+      package_dir={'utilise': 'src/utilise'},
+      include_package_data=True,
+      exclude_package_data={'': ['*tests/*']},
       install_requires=['setuptools'])
